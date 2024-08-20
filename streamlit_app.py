d37791705cf0b9e8a788a7238798958bc34d8b6d
João Oliveira <vitor191291@gmail.com>
2024-08-20 18:24:25 +0000

    initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+submit_agent_pg = st.Page("submit_agent.py", title="Submit New", icon=":material/add:")
+search_agent = st.Page("search_agents.py", title="Find Agents", icon=":material/search:")
+
+pg = st.navigation([submit_agent_pg, search_agent])
+
+st.set_page_config(page_title="Leaderboard", page_icon=":material/leaderboard:")
+
+st.title("Welcome to AI agents leaderboard!")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+pg.run()
